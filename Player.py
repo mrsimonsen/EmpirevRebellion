2b79344dd22d027aaa676dc6b992bcb8828d1237
mrsimonsen <johnsimonsen@weber.edu>
2021-04-03 20:08:18 -0600

    working on Player

--- Player.py
+++ Player.py
@@ -1,9 +1,81 @@
+from random import shuffle
 class Player():
+	OPTIONS = ResourceCard.Resource.
 	def __init__(self, name = "Computer", faction = "Imperial Faction"):
 		self.name = name
 		self.faction = faction
-		self.reserve = []
-		self.resource = ResourseCards.Resource()
-		self.strategy = StratageyCards.Stratagey()
+		self.points = 0
+		self.strategy = StratageyCard.Stratagey()
 		self.influence = 2
+		self.discard = []
+		self.deck = ResourseCard.Resource()
+		self.reserve = ResourceCard.Reserve()
+		self.hand = []
+		#choose 4 character cards to add to deck, the rest go in the reserve
+		for i in range(4):
+			self.add_character()
+		shuffle(self.deck)
 
+	def add_character(self):
+		print("Avaliable character cards to add:")
+		if not len(self.reserve):
+			print("No characters are left in your reserve!")
+		else:
+			print(self.reserve)
+			card = ''
+			while not card:
+				choice = input("What Reserve Character do you want to add?\n")
+				try:
+					i = int(choice)
+					if (i-1) < 0:
+						raise IndexError
+					card = self.reserve[i-1]
+				except ValueError:
+					print("That wasn't a number!")
+				except IndexError:
+					print("That wasn't a valid card number!")
+			self.deck.append(card)
+			self.reserve.remove(card)
+
+	def gain_influence(self, num):
+		self.influence += num
+	
+	def use_influence(self, num):
+		self.influence -= num
+		if self.influence < 1:
+			self.influence = 0
+
+	@property
+	def value(self):
+		value = 0
+		for card in self.hand:
+			value += card.value
+		return value
+
+	def view_hand(self):
+		print(f"You currently have {len(self.hand) cards in play for a total value of {self.value}.")
+		for card in self.hand:
+			print(card)
+	
+	def play_a_card(self):
+		print("You play a card:")
+		card = self.deck.pop(0)
+		self.hand.append(card)
+		print(card)
+		print(f"Your current resource value is {self.value}")
+
+	def spend_influence(self):
+		print("You choose to spend an Influence token to ready a card.")
+		if not self.influence:
+			print("You don't have any Influence tokens left to spend.")
+		else:
+			running = True
+			while running:
+				choice = input("What card would your like to recharge?\n").title()
+				if choice not in 
+				for card in self.hand:
+					if card.name == choice:
+						self.use_influence(1)
+						card.status == True
+						running == False
+						break
--- ResourceCard.py
+++ ResourceCard.py
@@ -2,21 +2,28 @@ import random as r
 class Card():
 	def __init__(self,name = None,ready = 0, exaust = 0, power = None):
 		self.name = name
-		self.ready = ready
-		self.exaust = exaust
+		self.ready_value = ready
+		self.exaust_value = exaust
 		self.power = power
 		# True = Ready, False = Exausted
 		self.status = True
+
+	@property
+	def value(self):
+		if self.status:
+			return self.ready_value
+		else:
+			return self.exaust_value
 	
 	def __str__(self):
 		rep = f"Card: {self.name}\nResource Value: "
 		if self.status:
-			rep += f"{self.ready}\nPower: {self.power}"
+			rep += f"{self.value}\nPower: {self.power}"
 		else:
-			rep += f"{self.exaust}\nPower: Exhausted"
+			rep += f"{self.value}\nPower: Exhausted"
 		return rep
 
-class Regular():
+class Resource():
 	# 16 regular resource cards per side split into 4 groups
 	RESOURCE = {
 		"Diplomacy": "Discard this card or 1 of your other resource cards of your choice.",
@@ -24,28 +31,23 @@ class Regular():
 		"Military": "Discard 1 of your opponet's resource cards of your choice.",
 		"Recon": "Look at the top 2 cards of either resource deck. Discard 1 of them and place the other on the top of that deck."
 		}
-	def __init__(self):
-		self.deck = []
+	def __init__(self, deck = []):
+		self.deck = deck
 		for name in self.RESOURCE:
 			for value in range(2,6):
-				self.add(name, value, value, self.RESOURCE[name])
+				self.deck.append(Card(name, value, value, self.RESOURCE[name]))
 		r.shuffle(self.deck)
-	
-	def add(self, *args):
-		name, rv, ev, power = args 
-		self.deck.append(Card(name, rv, ev, power))
 
-	def remove(self, card):
-		self.discard.append(card)
-		self.deck.remove(card)
 
-class Character():
+
+class Reserve():
 	#8 character resource cards per faction
 	REBELLION_CHARACTERS = {
 		"Chewbaca": "Exhaust up to 2 of your opponet's resource cards of your choice.",
 		"Han Solo": "Search your resource deck for 1 card of your choice and play it. Then shuffle your resource deck.",
 		"Lando Calrissian": "Discard any number of your resource cards of your choice. Then play an equal number of resource cards from the top of your deck.",
 		"Leia Organa": "Choose 1 character resource card from your reserve. Play that card or shuffle it into your resource deck.",
+		"Luke Skywalker": "Discard 1 of your resource cards of your choice and/or 1 of your opponent's resource cards of your choice.",
 		"Obi-Wan Kenobi": "Play 1 resource card of your choice from your discard pile.",
 		"R2-D2 and C-3PO": "Discard your chosen strategy card and choose a new strategy card.",
 		"Yoda": "Gain 1 influence token."
@@ -59,45 +61,38 @@ class Character():
 		"Grand Moff Tarkin": "Your opponent must discard 2 influence tokens.",
 		"IG-88": "Search either resource deck for 2 cards. Then discard 1 and place the other on top of that deck after shuffling it.",
 		"Jabba the Hutt": "Look at the top card of your resource deck. You may discard 1 of your resource cards and/or play the top resource card of your deck."
-		}		
+		}
 	
-	def __init__(self):
+	def __init__(self, faction = "Imperial Faction"):
+		self.reserve = []
+		self.faction = faction
 		if self.faction == "Rebel Faction":
 			characters = list(self.REBELLION_CHARACTERS.keys())
 			for i in range(8):
-				x = chracters.pop(0)
+				if len(characters) > 1:
+					x = characters.pop(0)
+				else:
+					x = characters[0]
 				self.reserve.append(Card(x,6,1,self.REBELLION_CHARACTERS[x]))
 		else:
 			characters = list(self.EMPIRE_CHARACTERS.keys())
 			for i in range(8):
-				x = characters.pop(0)
+				if len(characters) > 1:
+					x = characters.pop(0)
+				else:
+					x = characters[0]
 				self.reserve.append(Card(x,6,1,self.EMPIRE_CHARACTERS[x]))
-				print("Select 4 character resource cards to add to your deck")
-		for i in range(4):
-			self.add_character()
-
-		def add_character(self):
-		print("Avaliable character cards to add:")
-		if not len(self.reserve):
-			print("No characters are left in your reserve!")
-		else:
-			for i in range(len(self.reserve)):
-				print(f"{i+1} - {self.reserve[i].name}\n\t{self.reserve[i].power}")
-			card = ''
-			while not card:
-				choice = input("What Reserve Character do you want to add?\n")
-				try:
-					i = int(choice)
-					if (i-1) < 0:
-						raise IndexError
-					card = self.reserve[i-1]
-				except ValueError:
-					print("That wasn't a number!")
-				except IndexError:
-					print("That wasn't a valid card number!")
-			self.deck.append(card)
-			self.reserve.remove(card)
+		
+	def __str__(self):
+		rep = "Character card reserve:\n"
+		i = 1
+		for card in self.reserve:
+			rep += f"{i} -- {card.name}\n\t{card.power}\n"
+			i += 1
+		return rep
 
 
-x = Resource("Imperial Faction")
+
+
+x = Reserve("Rebel Faction")
 print(x)
